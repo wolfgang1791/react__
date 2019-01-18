8cc19b49aafcb57999c1bbb399667b10be4add56
Jonathan <jonathanperalta1893@gmail.com>
2019-01-17 20:06:03 -0500

    this.state setState

--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -1,25 +1,59 @@
-import React,{Component} from 'react';
+import React,{Component} from 'react'; // parte todo
 import WheatherData from './WeatherData';
 import Location from './Location';
 import './style.css'
-import {SUN} from '../../constants/weathers';
+import {SUN, WINDY} from '../../constants/weathers';
 
 
-const data ={
+const data1 ={
     temperature:80,
     weatherstate:SUN,
     humidity:50,
     winds:'10 m/s',
 }
 
+const data2 ={
+    temperature:15,
+    weatherstate:WINDY,
+    humidity:12,
+    winds:'0.5 m/s',
+}
+
 class WheatherLocation extends Component{
        
-    
-        render = () =>  (
-            <div className="weatherLocationCont">
-                <Location city={'Privet Drive'}/> 
-                <WheatherData data={data}/>
-            </div>
-        );
+        constructor(){
+            super();
+            this.state = {
+                city: 'Privet Drive',
+                data: data1,
+            };
+        }
+
+        handleUpdateClick = ()=>{
+            console.log("Con esa mirada tan hiriente");
+            
+            if(this.state.data === data1){
+                this.setState({
+                    city: 'Surey',
+                    data:data2,
+                });
+            }    
+            else{
+                this.setState({
+                   city: 'Privet Drive',
+                data: data1,
+                });
+            }
+        }
+        render = () =>  {
+            const {city,data} = this.state;
+            return(
+                <div className="weatherLocationCont">
+                    <Location city={city}/> 
+                    <WheatherData data={data}/>
+                    <button onClick={this.handleUpdateClick}>Actualizar</button>
+                </div>
+            )
+        };
 }
 export default WheatherLocation;
