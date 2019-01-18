006885caa987936c02f4d16122e5d6356a0a189b
Jonathan <jonathanperalta1893@gmail.com>
2019-01-17 19:45:02 -0500

    obj data

--- weatherapp/src/components/WeatherLocation/WeatherData/index.js
+++ weatherapp/src/components/WeatherLocation/WeatherData/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import WheatherExtraInfo from './WheatherExtraInfo';
 import WheatherTemperature from './WheatherTemperature';
 //import {WINDY,SNOW,RAIN,SUN,CLOUDY,CLOUD} from '../../../constants/weathers';
-import {SNOW} from '../../../constants/weathers';
+import PropTypes from 'prop-types';
 import './style.css';
 
-const WheatherData = () => (
+const WheatherData = ({data}) => { //arrow function
+
+    const {temperature,weatherstate,humidity,winds} = data;
+
+    return (
     <div className="weatherDataCont">
-        <WheatherTemperature temperature={20} weatherstate={SNOW}/>
-        <WheatherExtraInfo humidity={80} winds={'10m/s'}/>
+        <WheatherTemperature temperature={temperature} weatherstate={weatherstate}/>
+        <WheatherExtraInfo humidity={humidity} winds={winds}/>
     </div>
-)
+    )
+}
+
+WheatherData.propTypes = {
+    data:PropTypes.shape({
+        temperature:PropTypes.number.isRequired,
+        weatherstate:PropTypes.string.isRequired,
+        humidity:PropTypes.number.isRequired,
+        winds:PropTypes.string.isRequired,
+    }),
+}
 
 export default WheatherData;
--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import WheatherData from './WeatherData';
 import Location from './Location';
 import './style.css'
+import {SUN} from '../../constants/weathers';
+
+
+const data ={
+    temperature:80,
+    weatherstate:SUN,
+    humidity:50,
+    winds:'10 m/s',
+}
 
 const WheatherLocation = () =>  (
     <div className="weatherLocationCont">
         <Location city={'Privet Drive'}/> 
-        <WheatherData/>
+        <WheatherData data={data}/>
     </div>
 );
 
