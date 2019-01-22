2a06328d8639d02097ef823fc4204b65aa1bbc7d
Jonathan <jonathanperalta1893@gmail.com>
2019-01-21 20:47:52 -0500

    funciones forecstextended

--- weatherapp/src/components/ForecastExtended.js
+++ weatherapp/src/components/ForecastExtended.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 import './styles.css';
-
+/*
 const days = ['Lunes','Martes','Miercoles','Jueves','Viernes'];
 
 const data = {
@@ -10,23 +10,36 @@ const data = {
     humidity: 10,
     weatherstate:'normal',
     winds:'normal'
-}
+}*/
 
 class ForecastExtended extends Component{ 
 
+    constructor(){
+        super();
+        this.state = {
+            forecastdata : null,
+        }
+    }
+
     renderForecastItemDays = () => {
-        return days.map(day => (
+        return 'render items';
+       /* return days.map(day => (
             <ForecastItem key={day} weekDay={day} hour={10} data={data}></ForecastItem>
-        ));
+        ));*/
+    }
+
+    renderProgress = () => {
+        return 'cargando...';
     }
 
     render(){
         
-        const {city} = this.props;     
+        const {city} = this.props;
+        const {forecastdata} = this.state;
         return(
             <div>
                 <h3 className='forecast-title'>Pronóstico Extendido para {city}</h3>
-                {this.renderForecastItemDays()}
+                {forecastdata?this.renderForecastItemDays():this.renderProgress()}
             </div>
         );
     }
