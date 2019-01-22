c73e3618003380f1387b4cc7cb82d572ca3e627c
Jonathan <jonathanperalta1893@gmail.com>
2019-01-21 23:06:24 -0500

    weekday hour data

--- weatherapp/src/components/ForecastExtended.js
+++ weatherapp/src/components/ForecastExtended.js
@@ -2,8 +2,7 @@ import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 //import ForecastItem from './ForecastItem';
 import {api_id,url_forecast} from '../constants/api';
-//import {transformForecast} from '../services/transformForecast';
-import moment from 'moment';
+import transformForecast from '../services/transformForecast';
 import './styles.css';
 
 class ForecastExtended extends Component{ 
@@ -14,18 +13,7 @@ class ForecastExtended extends Component{
             forecastData : null,
         }
     }
-
-    transformForecast = data => {
-        
-        return data.list.filter(item =>(    
-      
-            moment.unix(item.dt).hour() === 4||
-            moment.unix(item.dt).hour() === 13 ||
-            moment.unix(item.dt).hour() === 16
-      
-        ))
-    };
-    
+ 
     componentDidMount=()=>{
         const api = `${url_forecast}${this.props.city}&appid=${api_id}`;
         
@@ -33,7 +21,7 @@ class ForecastExtended extends Component{
             return data.json();
         }).then(weather_data =>{
             console.log(weather_data);
-            const forecastData = this.transformForecast(weather_data);
+            const forecastData = transformForecast(weather_data);
             console.log(forecastData);
             this.setState({
                 forecastData
--- weatherapp/src/services/transformForecast.js
+++ weatherapp/src/services/transformForecast.js
@@ -1,7 +1,21 @@
 
+import transformWeather from '../services/transformWeather';
+import moment from 'moment';
+import 'moment/locale/es'
 
 const transformForecast = data => {
-    return {};
+        
+    return data.list.filter(item =>(    
+  
+        moment.unix(item.dt).hour() === 4||
+        moment.unix(item.dt).hour() === 13 ||
+        moment.unix(item.dt).hour() === 16
+  
+    )).map(item => ({
+        weekDay:moment.unix(item.dt).format('ddd'),
+        hour:moment.unix(item.dt).hour(),
+        data:transformWeather(item)
+    }))
 };
 
 export default transformForecast;
