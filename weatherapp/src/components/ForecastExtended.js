20c0ddf66122edae4086c34c215f37b52eed5452
Jonathan <jonathanperalta1893@gmail.com>
2019-01-21 22:54:01 -0500

    moment unix

--- weatherapp/src/components/ForecastExtended.js
+++ weatherapp/src/components/ForecastExtended.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 //import ForecastItem from './ForecastItem';
 import {api_id,url_forecast} from '../constants/api';
-import {transformForecast} from '../services/transformForecast';
+//import {transformForecast} from '../services/transformForecast';
+import moment from 'moment';
 import './styles.css';
 
 class ForecastExtended extends Component{ 
@@ -10,10 +11,21 @@ class ForecastExtended extends Component{
     constructor(){
         super();
         this.state = {
-            forecastdata : null,
+            forecastData : null,
         }
     }
 
+    transformForecast = data => {
+        
+        return data.list.filter(item =>(    
+      
+            moment.unix(item.dt).hour() === 4||
+            moment.unix(item.dt).hour() === 13 ||
+            moment.unix(item.dt).hour() === 16
+      
+        ))
+    };
+    
     componentDidMount=()=>{
         const api = `${url_forecast}${this.props.city}&appid=${api_id}`;
         
@@ -21,9 +33,10 @@ class ForecastExtended extends Component{
             return data.json();
         }).then(weather_data =>{
             console.log(weather_data);
-            const forecastData = transformForecast(weather_data);
+            const forecastData = this.transformForecast(weather_data);
+            console.log(forecastData);
             this.setState({
-                forecastdata : forecastData
+                forecastData
             });
         })
     }
