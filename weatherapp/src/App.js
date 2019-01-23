23a226f192e902641c2cd9e3fbc662d3a4ff2263
Jonathan <jonathanperalta1893@gmail.com>
2019-01-22 21:11:29 -0500

    LocationListContainer

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import {Grid,Row,Col} from 'react-flexbox-grid';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
 import Appbar from 'material-ui/AppBar';
-import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
-import {setCity} from './actions/';
+import LocationListContainer from './containers/LocationListContainer';
 import './App.css';
 
 const cities = ['London,uk','Lima,pe','Madrid,es','Moscu,rus','Guildford,uk','Glasgow,uk'];
@@ -21,17 +18,8 @@ class App extends Component {
         };
     }
 
-
-  handleSelectedLocation = ({city}) =>{
-      this.setState({
-        city
-      });
-     // console.log(`handleSelectionLocation ${city}`);
-      this.props.dispatchsetCity(city);
-  }  
-
   render() {
-      let {city} = this.state;
+      const {city} = this.state;
       return (
           <MuiThemeProvider>
             <Grid>
@@ -42,7 +30,7 @@ class App extends Component {
                 </Row>
                 <Row>
                     <Col xs={12} md={6}>
-                        <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}></LocationList>
+                        <LocationListContainer cities={cities}></LocationListContainer> 
                     </Col>
                     <Col xs={12} md={6}>
                         <Paper zDepth={4}>
@@ -61,18 +49,4 @@ class App extends Component {
 }
 
 
-App.propTypes = {
-    setCity : PropTypes.func.isRequired,
-}
-
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        dispatchsetCity: value => dispatch(setCity(value))
-    }
-}
-
-export default connect(null,mapDispatchToProps)(App);
-//const AppConnected = connect(null,mapDispatchToProps)(App);
-
-//export default AppConnected;
+export default App;
